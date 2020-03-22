e0e1f7291bd25ff121f5cea2691b52270e9cd97a
Nassuel <valeran1@seattleu.edu>
2020-03-22 02:39:37 -0700

    Adding argparse module to parse arguments for mr and hr input

--- MHW_BackUpFiles/MHW_file_mv.py
+++ MHW_BackUpFiles/MHW_file_mv.py
@@ -1,8 +1,8 @@
 import os
 import shutil
-from variables import file_location
+import argparse
 from datetime import datetime
-
+from variables import file_location
 
 class BackUpper:
     _master_rank = ''
@@ -15,26 +15,26 @@ class BackUpper:
         self._dict_from_file = dict_from_file
 
     def copy_save_data(self, f_origin_path, f_dest_path):
-        try: 
+        try:
             shutil.copytree(f_origin_path, f_dest_path)
             print("File copied successfully.")
-        
-        # If there is any permission issue 
-        except PermissionError: 
-            print("Permission denied.") 
-        
-        # For other errors 
-        except: 
+
+        # If there is any permission issue
+        except PermissionError:
+            print("Permission denied.")
+
+        # For other errors
+        except:
             print("Error occurred while copying file.")
 
-        return 
+        return
 
     def clean_up_old_files(self, dest_path):
         cleanup_path = self._dict_from_file['in_dest_path']
         cleanup_dst_path = self._dict_from_file['in_cleanup_dest_path']
         file_to_not_move = dest_path.split('/')[-1]
         files_in_dir = os.listdir(cleanup_path)
-        
+
         for file in files_in_dir:
             files_to_move = cleanup_path + file
             if file_to_not_move != file:
@@ -46,22 +46,31 @@ class BackUpper:
 
     def run(self):
         today_dt = datetime.now()
-        full_dest_path = self._dict_from_file['in_dest_path'] + '{0}({1},{2})'.format(today_dt.strftime('%d%m%Y_%H,%M,%S'), self._master_rank, self._hunter_rank)
+        full_dest_path = self._dict_from_file['in_dest_path'] + '{0}({1},{2})'.format(
+            today_dt.strftime('%d%m%Y_%H,%M,%S'), self._master_rank, self._hunter_rank)
 
         self.copy_save_data(self._dict_from_file['in_origin'], full_dest_path)
-        self. clean_up_old_files(full_dest_path)
+        self.clean_up_old_files(full_dest_path)
+
 
 def main(dictionary):
-    current_master_rnk = '178'
-    current_hunter_rnk = '207'
+    current_master_rnk = dictionary['mr']
+    current_hunter_rnk = dictionary['hr']
     # print(dictionary)
-    backupper_obj = BackUpper(current_master_rnk, current_hunter_rnk, dictionary['variables'])
+    backupper_obj = BackUpper(
+        current_master_rnk, current_hunter_rnk, dictionary['file_location'])
 
     backupper_obj.run()
 
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Parse command line argument')
+    parser.add_argument('-hr', type=int, required=True)
+    parser.add_argument('-mr', type=int, required=True)
+    args = parser.parse_args()
     input_to_main = {}
-    # print(file_location)
-    input_to_main['variables'] = file_location
+    input_to_main['file_location'] = file_location
+    input_to_main['hr'] = args.hr
+    input_to_main['mr'] = args.mr
 
     main(input_to_main)
