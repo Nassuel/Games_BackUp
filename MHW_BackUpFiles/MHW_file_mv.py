530436feb6e50f8aeffca4cacb71636969dfcc08
Nassuel <valeran1@seattleu.edu>
2020-09-18 01:42:50 -0700

    Super close, just need to figure out why the clean_up_old_files is not moving the zip file into the folder

--- MHW_BackUpFiles/MHW_file_mv.py
+++ MHW_BackUpFiles/MHW_file_mv.py
@@ -3,7 +3,7 @@ import shutil
 import zipfile as zipf
 import argparse
 from datetime import datetime
-from test_variables import file_location
+from variables import file_location
 
 
 class BackUpper:
@@ -16,7 +16,7 @@ class BackUpper:
     _in_dest_path = ''
     _in_cleanup_dest_path = ''
 
-    def __init__(self, master_rank, hunter_rank, dict_from_file, dry_run=0):
+    def __init__(self, master_rank, hunter_rank, dict_from_file, dry_run):
         self._master_rank = master_rank
         self._hunter_rank = hunter_rank
 
@@ -26,87 +26,61 @@ class BackUpper:
         self._in_dest_path = dict_from_file['in_dest_path']
         self._in_cleanup_dest_path = dict_from_file['in_cleanup_dest_path']
 
-    def copy_save_data(self, f_origin_path, f_dest_path):
-        try:
-            shutil.copytree(f_origin_path, f_dest_path)
-            print("File copied successfully.")
-        # If there is any permission issue
-        except PermissionError:
-            print("Permission denied.")
-        # For other errors
-        except:
-            print("Error occurred while copying file.")
+        self.full_dest_path = self._in_dest_path + '{0}({1},{2})'.format(
+            datetime.now().strftime('%d%m%Y_%H,%M,%S'), self._master_rank, self._hunter_rank
+        )
 
+    def _copy_save_data(self):
+        shutil.make_archive(self.full_dest_path, 'zip', self._in_origin)
         return
 
-    def clean_up_old_files(self, dest_path):
+    def _clean_up_old_files(self):
         cleanup_path = self._in_dest_path
         cleanup_dst_path = self._in_cleanup_dest_path
-        file_to_not_move = dest_path.split('/')[-1]
+        file_to_not_move = self.full_dest_path.split('/')[-1]
         files_in_dir = os.listdir(cleanup_path)
 
         for file in files_in_dir:
             file_to_move_path = cleanup_path + file
             if file_to_not_move != file:
                 # print(file_to_not_move)
-                shutil.move(file_to_move_path, cleanup_dst_path)
+                # shutil.move(file_to_move_path, cleanup_dst_path)
+                shutil.make_archive(cleanup_dst_path, 'zip', file_to_move_path)
 
         print('Files have been cleaned up successfully.')
         return
 
-    def zip_file_copy(self, f_origin_path, f_dest_path):
-        try:
-            import zlib
-            comp = zipf.ZIP_DEFLATED
-        except:
-            comp = zipf.ZIP_STORED
-
-        zf = zipf.ZipFile(f_dest_path+".zip", mode='w')
-        try:
-            for file in os.listdir(f_origin_path):
-                file_to_move_path = f_origin_path + file
-                zf.write(file_to_move_path, compress_type=comp)
-        except:
-            print('Error when writing folder to zip')
-        finally:
-            zf.close()
-
-        return
-
     def run(self):
         if not self.dry_run:
-            today_dt = datetime.now()
-            full_dest_path = self._in_dest_path + '{0}({1},{2})'.format(
-                today_dt.strftime('%d%m%Y_%H,%M,%S'), self._master_rank, self._hunter_rank)
-
-            # self.copy_save_data(self._in_origin, full_dest_path)
-            self.zip_file_copy(self._in_origin, full_dest_path)
-            self.clean_up_old_files(full_dest_path)
+            self._copy_save_data()
+            self._clean_up_old_files()
         else:
-            print('You\' currently doing a dry run!')
+            print('You\'re currently doing a dry run!')
 
+# def main(dictionary):
+#     # print(dictionary)
+#     backupper_obj = BackUpper(
+#         dictionary['mr'], dictionary['hr'], dictionary['file_location'], dictionary['dryrun']
+#     )
 
-def main(dictionary):
-    current_master_rnk = dictionary['mr']
-    current_hunter_rnk = dictionary['hr']
-    # print(dictionary)
-    backupper_obj = BackUpper(
-        current_master_rnk, current_hunter_rnk, dictionary['file_location']
-    )
-
-    backupper_obj.run()
-
+#     backupper_obj.run()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Parse command line argument for BackUpper program')
+    parser = argparse.ArgumentParser(
+        description='Parse command line argument for BackUpper program')
     parser.add_argument('-hr', type=int)
     parser.add_argument('-mr', type=int)
-    parser.add_argument('-dryrun', type=str, required=False)
+    parser.add_argument('-dryrun', type=str, required=False, default=0)
     args = parser.parse_args()
     input_to_main = dict(
         file_location=file_location['file_locations'], hr=args.hr, mr=args.mr, dryrun=args.dryrun
     )
 
     # print(input_to_main)
+    # main(input_to_main)
 
-    main(input_to_main)
+    backupper_obj = BackUpper(
+        args.mr, args.hr, file_location['file_locations'], args.dryrun
+    )
+
+    backupper_obj.run()
